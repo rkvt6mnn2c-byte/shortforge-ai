0a2d2247cd14da94193dc2b8a80bbbee5645ba9b
Owen Meshekow <owenm@MacBookPro-215.lan>
2026-05-25 22:28:28 -0700

    fix stripe portal route duplication

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const supabaseAdmin = createClient(
 );
 
 const app = express();
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 const PORT = process.env.PORT || 3001;
 
